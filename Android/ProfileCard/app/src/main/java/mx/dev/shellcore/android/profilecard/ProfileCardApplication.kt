9ea120629c3e1aa21f4bf4e9d13028a43ed0d402
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-03-08 19:17:07 -0700

    Inicialización de inyección de dependencias

---
+++ Android/ProfileCard/app/src/main/java/mx/dev/shellcore/android/profilecard/ProfileCardApplication.kt
@@ -0,0 +1,7 @@
+package mx.dev.shellcore.android.profilecard
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ProfileCardApplication: Application()
