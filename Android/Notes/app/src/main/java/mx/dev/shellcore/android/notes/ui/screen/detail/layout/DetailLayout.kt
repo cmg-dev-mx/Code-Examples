be441ceed7b211e6b4d9d396a78bee58d576016e
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-04-22 11:37:48 -0600

    Implementación de pantalla de detalle de nota

--- Android/Notes/app/src/main/java/mx/dev/shellcore/android/notes/ui/screen/detail/layout/DetailLayout.kt
+++ Android/Notes/app/src/main/java/mx/dev/shellcore/android/notes/ui/screen/detail/layout/DetailLayout.kt
@@ -1,19 +1,36 @@
 package mx.dev.shellcore.android.notes.ui.screen.detail.layout
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import mx.dev.shellcore.android.notes.R
 import mx.dev.shellcore.android.notes.ui.theme.NotesTheme
 
 @Preview
@@ -32,6 +49,10 @@ fun DetailLayout() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailLayoutContent() {
+
+    var title by remember { mutableStateOf("") }
+    var content by remember { mutableStateOf("") }
+
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
@@ -43,6 +64,15 @@ fun DetailLayoutContent() {
                 ),
                 title = {
                     Text(text = "Detalle de Nota")
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Go back",
+                            tint = MaterialTheme.colorScheme.onPrimary
+                        )
+                    }
                 }
             )
         }
@@ -51,9 +81,60 @@ fun DetailLayoutContent() {
             modifier = Modifier
                 .padding(paddingValues)
                 .fillMaxSize()
-                .padding(126.dp)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
+            Text(
+                text = stringResource(R.string.title),
+                style = MaterialTheme.typography.labelMedium
+            )
+            TextField(
+                modifier = Modifier.fillMaxWidth(),
+                singleLine = true,
+                placeholder = {
+                    Text(text = stringResource(R.string.title_placeholder))
+                },
+                value = title,
+                onValueChange = {
+                    title = it
+                }
+            )
 
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+
+            Text(
+                text = stringResource(R.string.content),
+                style = MaterialTheme.typography.labelMedium
+            )
+
+            TextField(
+                modifier = Modifier.fillMaxWidth(),
+                minLines = 5,
+                placeholder = {
+                    Text(text = stringResource(R.string.content_placeholder))
+                },
+                value = content,
+                onValueChange = {
+                    content = it
+                }
+            )
+
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            )
+
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { /*TODO*/ }
+            ) {
+                Text(text = stringResource(R.string.save))
+            }
         }
     }
 }
