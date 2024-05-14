2717469a72f4d6432ebd723f1a8b1b0956a0564f
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-05-14 09:45:47 -0600

    Implementación de HCaptcha

--- Android/HCaptcha/app/src/main/java/mx/dev/shellcore/android/hcaptcha/ui/screen/main/layout/MainLayout.kt
+++ Android/HCaptcha/app/src/main/java/mx/dev/shellcore/android/hcaptcha/ui/screen/main/layout/MainLayout.kt
@@ -1,5 +1,9 @@
 package mx.dev.shellcore.android.hcaptcha.ui.screen.main.layout
 
+import android.app.Activity
+import android.content.Intent
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,10 +16,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import mx.dev.shellcore.android.hcaptcha.HCaptchaActivity
 import mx.dev.shellcore.android.hcaptcha.ui.theme.HCaptchaTheme
-import kotlin.random.Random
 
 @Preview
 @Composable
@@ -29,6 +34,23 @@ private fun MainLayoutPreview() {
 fun MainLayout() {
     var token by remember { mutableStateOf("") }
 
+    val context = LocalContext.current
+    val intent = Intent(context, HCaptchaActivity::class.java)
+    val launcher = rememberLauncherForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        val data: Intent? = result.data
+        when (result.resultCode) {
+            Activity.RESULT_OK -> {
+                token = data?.getStringExtra("token") ?: ""
+            }
+
+            Activity.RESULT_CANCELED -> {
+                token = "Error"
+            }
+        }
+    }
+
     Column(
         modifier = Modifier
             .padding(top = 16.dp)
@@ -37,7 +59,7 @@ fun MainLayout() {
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         Button(onClick = {
-            token = Random.nextInt(0, 100).toString()
+            launcher.launch(intent)
         }) {
             Text(text = "Validate HCaptcha")
         }
