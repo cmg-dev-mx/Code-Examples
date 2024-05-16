f745058870c059431dabd421bf8ec556b5494bc6
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-05-16 14:15:05 -0600

    Despliegue de los datos recibidos por el servicio en la vista

--- React-Native/01-react-foundation/src/components/UsersPage.tsx
+++ React-Native/01-react-foundation/src/components/UsersPage.tsx
@@ -1,20 +1,24 @@
 import axios from "axios"
-import { useEffect } from "react"
-import { ReqResUserListResponse } from "../interfaces";
+import { useEffect, useState } from "react"
+import { ReqResUserListResponse, User } from "../interfaces";
 
-const loadUsers = async () => {
+const loadUsers = async (): Promise<User[]> => {
     try {
         const { data } = await axios.get<ReqResUserListResponse>('https://reqres.in/api/users');
         return data.data;
     } catch (error) {
         console.log(error);
+        return [];
     }
 }
 
 export const UsersPage = () => {
 
-    useEffect(() => {        
-        loadUsers().then(users => { console.log(users) });
+    const [users, setUsers] = useState<User[]>([]);
+
+    useEffect(() => {
+        loadUsers()
+            .then(setUsers);
     }, [])
 
     return (
@@ -29,13 +33,32 @@ export const UsersPage = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>avatar</td>
-                        <td>nombre</td>
-                        <td>email</td>
-                    </tr>
+                    {
+                        users.map(user => (
+                            <UserRow key={user.id} user={user} />
+                        ))
+                    }
+
                 </tbody>
             </table>
         </>
+    )
+}
+
+interface Props {
+    user: User
+}
+
+export const UserRow = ({ user }: Props) => {
+
+    const { avatar, first_name, last_name, email } = user;
+
+    return (
+        <tr>
+            <td><img style={{ width: '50px' }} src={avatar} alt="User avatar" /></td>
+            <td>{first_name} {last_name}</td>
+            <td>{email}</td>
+        </tr>
+
     )
 }
