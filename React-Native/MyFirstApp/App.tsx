b1e37d76d761cddbe352a533846cb0ce077328dd
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-06-19 10:07:26 -0600

    Personalización sencilla de la vista principal

--- React-Native/MyFirstApp/App.tsx
+++ React-Native/MyFirstApp/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {SafeAreaView, Text, View} from 'react-native';
+import {HelloWorldScreen} from './src/presentation/screens/HelloWorldScreen';
 
 export const App = () => {
   return (
-    <View>
-      <Text>Hola Mundo</Text>
-    </View>
+    <SafeAreaView style={{flex: 1}}>
+      <HelloWorldScreen />
+    </SafeAreaView>
   );
 };
---
+++ React-Native/MyFirstApp/src/presentation/screens/HelloWorldScreen.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export const HelloWorldScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 45,
+    textAlign: 'center',
+    color: 'black',
+    padding: 20,
+  },
+});
