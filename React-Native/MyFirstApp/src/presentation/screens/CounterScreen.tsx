efd70ecf4b057beb4c3d4d914033e659eb0a2459
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-06-19 11:17:33 -0600

    Personalización de botón por plataforma

--- React-Native/MyFirstApp/src/presentation/screens/CounterScreen.tsx
+++ React-Native/MyFirstApp/src/presentation/screens/CounterScreen.tsx
@@ -1,5 +1,12 @@
 import React, {useState} from 'react';
-import {Button, Pressable, StyleSheet, Text, View} from 'react-native';
+import {
+  Button,
+  Platform,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
 export const CounterScreen = () => {
   const [count, setCount] = useState(0);
@@ -8,8 +15,11 @@ export const CounterScreen = () => {
     <View style={styles.container}>
       <Text style={styles.title}> {count} </Text>
 
-      <Pressable onPress={() => setCount(count + 1)}>
-        <Text>+1</Text>
+      <Pressable
+        onPress={() => setCount(count + 1)}
+        onLongPress={() => setCount(0)}
+        style={({pressed}) => [styles.button, pressed && styles.buttonPressed]}>
+        <Text style={styles.buttonText}>Increment</Text>
       </Pressable>
     </View>
   );
@@ -25,5 +35,18 @@ const styles = StyleSheet.create({
     fontSize: 80,
     color: 'black',
     fontWeight: '300',
+  },
+  button: {
+    backgroundColor: Platform.OS === 'android' ? '#5856D6' : '#FF9427',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 10,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: 'white',
+  },
+  buttonPressed: {
+    backgroundColor: Platform.OS === 'android' ? '#4746AB' : '#D27F0B',
   },
 });
