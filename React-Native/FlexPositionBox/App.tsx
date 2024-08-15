ec4fd4255398afcf22d1e45389d4e09bd0c4279c
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-08-15 15:20:59 -0600

    Ejemplo de Dimensiones y tamaños porcentuales

--- React-Native/FlexPositionBox/App.tsx
+++ React-Native/FlexPositionBox/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {SafeAreaView} from 'react-native';
 import {MainScreen} from './src/presentation/screen/MainScreen';
 import {BoxObjectModelScreen} from './src/presentation/screen/BoxObjectModelScreen';
+import {DimensionScreen} from './src/presentation/screen/DimensionScreen';
 
 export const App = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
       {/* <MainScreen /> */}
-      <BoxObjectModelScreen />
+      {/* <BoxObjectModelScreen /> */}
+      <DimensionScreen />
     </SafeAreaView>
   );
 };
