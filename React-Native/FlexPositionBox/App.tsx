768e6b8abd7849bf28123b0766db0695b6227b2b
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-08-16 08:49:54 -0600

    Ejemplo de posiciones relativas

--- React-Native/FlexPositionBox/App.tsx
+++ React-Native/FlexPositionBox/App.tsx
@@ -3,13 +3,15 @@ import {SafeAreaView} from 'react-native';
 import {MainScreen} from './src/presentation/screen/MainScreen';
 import {BoxObjectModelScreen} from './src/presentation/screen/BoxObjectModelScreen';
 import {DimensionScreen} from './src/presentation/screen/DimensionScreen';
+import {PositionScreen} from './src/presentation/screen/PositionScreen';
 
 export const App = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
       {/* <MainScreen /> */}
       {/* <BoxObjectModelScreen /> */}
-      <DimensionScreen />
+      {/* <DimensionScreen /> */}
+      <PositionScreen />
     </SafeAreaView>
   );
 };
