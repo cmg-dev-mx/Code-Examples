9b78392002095ec787042f84682d36afd0c66fb8
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-09-16 15:10:00 -0600

    Personalización del drawer

--- React-Native/05-Navigation/src/presentation/routes/SideMenuNavigator.tsx
+++ React-Native/05-Navigation/src/presentation/routes/SideMenuNavigator.tsx
@@ -1,6 +1,13 @@
-import {createDrawerNavigator} from '@react-navigation/drawer';
+import {
+  createDrawerNavigator,
+  DrawerContentComponentProps,
+  DrawerContentScrollView,
+  DrawerItemList,
+} from '@react-navigation/drawer';
 import {StackNavigator} from './StackNavigator';
 import {ProfileScreen} from '../screens/profile/ProfileScreen';
+import {globalColors} from '../theme/theme';
+import {Text, useWindowDimensions, View} from 'react-native';
 
 export type DrawerRootStackParams = {
   StackNavigator: undefined;
@@ -10,10 +17,41 @@ export type DrawerRootStackParams = {
 const Drawer = createDrawerNavigator<DrawerRootStackParams>();
 
 export const SideMenuNavigator = () => {
+  const dimensions = useWindowDimensions();
+
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      drawerContent={props => <CustomDrawerContent {...props} />}
+      screenOptions={{
+        drawerType: dimensions.width >= 750 ? 'permanent' : 'slide',
+        headerShown: false,
+        drawerActiveBackgroundColor: globalColors.primary,
+        drawerActiveTintColor: 'white',
+        drawerInactiveTintColor: globalColors.primary,
+        drawerItemStyle: {
+          borderRadius: 100,
+          paddingHorizontal: 20,
+        },
+      }}>
       <Drawer.Screen name="StackNavigator" component={StackNavigator} />
       <Drawer.Screen name="Profile" component={ProfileScreen} />
     </Drawer.Navigator>
+  );
+};
+
+const CustomDrawerContent = (props: DrawerContentComponentProps) => {
+  return (
+    <DrawerContentScrollView>
+      <View
+        style={{
+          height: 200,
+          backgroundColor: globalColors.primary,
+          margin: 30,
+          borderRadius: 50,
+        }}
+      />
+
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
   );
 };
