dd95597764cb45a01ba8c7b8d28eb4f797ce692d
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-09-16 15:16:27 -0600

    Implementación de vista profile con espaciado según el dispositivo

--- React-Native/05-Navigation/src/presentation/screens/profile/ProfileScreen.tsx
+++ React-Native/05-Navigation/src/presentation/screens/profile/ProfileScreen.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {globalStyles} from '../../theme/theme';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {PrimaryButton} from '../../components/shared/PrimaryButton';
+import {DrawerActions, useNavigation} from '@react-navigation/native';
 
 export const ProfileScreen = () => {
+  const insets = useSafeAreaInsets();
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View
+      style={{
+        flex: 1,
+        paddingHorizontal: 20,
+        marginTop: insets.top,
+      }}>
+      <Text
+        style={{
+          marginBottom: 10,
+        }}>
+        ProfileScreen
+      </Text>
+      <PrimaryButton
+        label="Abrir Menú"
+        onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
+      />
     </View>
   );
 };
