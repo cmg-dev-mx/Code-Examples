035f94f35f884ffe6386cab8c3d621e0454deacf
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-09-16 14:44:29 -0600

    Implementación de botón personalizado para abrir/cerrar el drawer

--- React-Native/05-Navigation/src/App.tsx
+++ React-Native/05-Navigation/src/App.tsx
@@ -2,8 +2,6 @@ import '../gesture-handler';
 
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {StackNavigator} from './presentation/routes/StackNavigator';
-import {createDrawerNavigator} from '@react-navigation/drawer';
 import {SideMenuNavigator} from './presentation/routes/SideMenuNavigator';
 
 export const App = () => {
--- React-Native/05-Navigation/src/presentation/screens/home/HomeScreen.tsx
+++ React-Native/05-Navigation/src/presentation/screens/home/HomeScreen.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text, Pressable} from 'react-native';
 import {globalStyles} from '../../theme/theme';
 import {PrimaryButton} from '../../components/shared/PrimaryButton';
-import {type NavigationProp, useNavigation} from '@react-navigation/native';
+import {
+  DrawerActions,
+  type NavigationProp,
+  useNavigation,
+} from '@react-navigation/native';
 import type {RootStackParams} from '../../routes/StackNavigator';
 
 export const HomeScreen = () => {
   const navigation = useNavigation<NavigationProp<RootStackParams>>();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <Pressable
+          onPress={() => {
+            navigation.dispatch(DrawerActions.toggleDrawer());
+          }}>
+          <Text>Menu</Text>
+        </Pressable>
+      ),
+    });
+  }, []);
 
   return (
     <View style={globalStyles.container}>
