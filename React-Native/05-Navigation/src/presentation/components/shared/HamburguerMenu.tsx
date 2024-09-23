539c61a2320183d741f9d526a3d66be8e1d64659
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-09-23 15:21:44 -0600

    Menú hamburguesa en tabs

---
+++ React-Native/05-Navigation/src/presentation/components/shared/HamburguerMenu.tsx
@@ -0,0 +1,20 @@
+import {DrawerActions, useNavigation} from '@react-navigation/native';
+import React, {useEffect} from 'react';
+import {Pressable, Text} from 'react-native';
+
+export const HamburguerMenu = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <Pressable
+          onPress={() => navigation.dispatch(DrawerActions.toggleDrawer)}>
+          <Text>Menú</Text>
+        </Pressable>
+      ),
+    });
+  }, []);
+
+  return <></>;
+};
