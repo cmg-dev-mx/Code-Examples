f8b032afd779dc3e583074da474dd222d5cb92d3
Cesar Morales Garduno <cesar.garduno@dish.com>
2024-09-24 14:19:23 -0600

    Adición de librería de íconos en Android

--- React-Native/05-Navigation/src/presentation/screens/tabs/Tab1Screen.tsx
+++ React-Native/05-Navigation/src/presentation/screens/tabs/Tab1Screen.tsx
@@ -1,13 +1,15 @@
-import {DrawerActions, useNavigation} from '@react-navigation/native';
-import React, {useEffect} from 'react';
-import {Pressable, Text, View} from 'react-native';
+import React from 'react';
+import {Text, View} from 'react-native';
 import {HamburguerMenu} from '../../components/shared/HamburguerMenu';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export const Tab1Screen = () => {
   return (
     <View>
       <HamburguerMenu />
       <Text>Tab1Screen</Text>
+      <Icon name="rocket" size={30} />
     </View>
   );
 };
