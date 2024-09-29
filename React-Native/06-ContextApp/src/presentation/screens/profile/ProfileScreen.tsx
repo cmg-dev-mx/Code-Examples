61c4cb508d6c56178728be6b4c074fbedab39e5f
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-09-28 18:20:25 -0600

    Creación de pantallas requeridas

---
+++ React-Native/06-ContextApp/src/presentation/screens/profile/ProfileScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {styles} from '../../../config';
+
+export const ProfileScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>ProfileScreen</Text>
+    </View>
+  );
+};
---
+++ React-Native/06-ContextApp/src/presentation/screens/settings/SettingsScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {styles} from '../../../config';
+
+export const SettingsScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>SettingsScreen</Text>
+    </View>
+  );
+};
