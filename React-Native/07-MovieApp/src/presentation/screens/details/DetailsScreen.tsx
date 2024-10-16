3443ff13011194dd11ccddca12c67acfb6e90a93
Cesar Morales Garduno <cesar.garduno@dish.com>
2024-10-16 11:05:47 -0600

    Implementación de encabezado de detalle

--- React-Native/07-MovieApp/src/presentation/screens/details/DetailsScreen.tsx
+++ React-Native/07-MovieApp/src/presentation/screens/details/DetailsScreen.tsx
@@ -4,17 +4,30 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import {RootStackParams} from '../../navigation/Navigation';
 import {useMovie} from '../../hooks/useMovie';
+import {MovieHeader} from '../../components/movie/MovieHeader';
 
 interface Props extends StackScreenProps<RootStackParams, 'Details'> {}
 
 export const DetailsScreen = ({route}: Props) => {
   const {movieId} = route.params;
 
-  const {} = useMovie(movieId);
+  const {isLoading, movie} = useMovie(movieId);
+
+  if (isLoading || !movie) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
 
   return (
     <View>
-      <Text>Details Screen</Text>
+      <MovieHeader
+        poster={movie.poster}
+        originalTitle={movie.originalTitle}
+        title={movie.title}
+      />
     </View>
   );
 };
