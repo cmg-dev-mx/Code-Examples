7f4e5378838e852fb46a20bc8aeb16671e88ca31
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-10-16 10:37:22 -0600

    Implementación de caso de uso y mapeo de detalle de película

--- React-Native/07-MovieApp/src/infrastructure/mappers/movie.mapper.ts
+++ React-Native/07-MovieApp/src/infrastructure/mappers/movie.mapper.ts
@@ -1,5 +1,8 @@
-import type {Movie} from '../../core/entities/movie.entity';
-import type {Result} from '../interfaces/movie-db.responses';
+import type {FullMovie, Movie} from '../../core/entities/movie.entity';
+import type {
+  MovieDetailResponse,
+  Result,
+} from '../interfaces/movie-db.responses';
 
 const baseUrl = 'https://image.tmdb.org/t/p/w500';
 
@@ -13,6 +16,25 @@ export class MovieMapper {
       rating: result.vote_average,
       poster: `${baseUrl}${result.poster_path}`,
       backdrop: `${baseUrl}${result.backdrop_path}`,
+    };
+  }
+
+  static fromMovieDBToEntity(response: MovieDetailResponse): FullMovie {
+    return {
+      id: response.id,
+      title: response.title,
+      description: response.overview,
+      releaseDate: response.release_date,
+      rating: response.vote_average,
+      poster: `${baseUrl}${response.poster_path}`,
+      backdrop: `${baseUrl}${response.backdrop_path}`,
+      genres: response.genres.map(genre => genre.name),
+      duration: response.runtime,
+      budget: response.budget,
+      originalTitle: response.original_title,
+      productionCompanies: response.production_companies.map(
+        company => company.name,
+      ),
     };
   }
 }
