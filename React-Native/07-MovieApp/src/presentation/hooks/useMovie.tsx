921da058a838a8382bafd424534da749d7ecbc0e
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-10-16 14:08:30 -0600

    Implementación de servico de consulta de actores

--- React-Native/07-MovieApp/src/presentation/hooks/useMovie.tsx
+++ React-Native/07-MovieApp/src/presentation/hooks/useMovie.tsx
@@ -2,10 +2,12 @@ import React, {useEffect, useState} from 'react';
 import {FullMovie} from '../../core/entities/movie.entity';
 import * as useCases from '../../core/use-cases';
 import {movieDBFetcher} from '../../config/adapters/movieDB.adapter';
+import {Cast} from '../../core/entities/cast.entity';
 
 export const useMovie = (movieId: number) => {
   const [isLoading, setIsLoading] = useState(true);
   const [movie, setMovie] = useState<FullMovie>();
+  const [cast, setCast] = useState<Cast[]>();
 
   useEffect(() => {
     loadMovie();
@@ -13,13 +15,23 @@ export const useMovie = (movieId: number) => {
 
   const loadMovie = async () => {
     setIsLoading(true);
-    const fullMovie = await useCases.getMovieById(movieDBFetcher, movieId);
+
+    const fullMoviePromise = useCases.getMovieById(movieDBFetcher, movieId);
+    const castPromise = useCases.getMovieCastUseCase(movieDBFetcher, movieId);
+
+    const [fullMovie, cast] = await Promise.all([
+      fullMoviePromise,
+      castPromise,
+    ]);
+
     setMovie(fullMovie);
+    setCast(cast);
     setIsLoading(false);
   };
 
   return {
     isLoading,
     movie,
+    cast,
   };
 };
