4ade59697de7321f62d66e36e1e03b9934c2c322
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-10-21 13:44:40 -0600

    Adición de librerías de íconos y navegación

--- React-Native/08-ComponentsApp/src/ComponentsApp.tsx
+++ React-Native/08-ComponentsApp/src/ComponentsApp.tsx
@@ -1,9 +1,14 @@
+import 'react-native-gesture-handler';
+import '../gesture-handler';
+
+import {NavigationContainer} from '@react-navigation/native';
 import {View, Text} from 'react-native';
+import {Navigator} from './presentation/navigator/Navigator';
 
 export const ComponentsApp = () => {
   return (
-    <View>
-      <Text>ComponentsApp</Text>
-    </View>
+    <NavigationContainer>
+      <Navigator />
+    </NavigationContainer>
   );
 };
