839df8f451cdd05dc0a628ccb2848ded71d1936b
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-10-25 13:16:07 -0600

    Implementación de switch customizado

--- React-Native/08-ComponentsApp/src/presentation/screens/switches/SwitchScreen.tsx
+++ React-Native/08-ComponentsApp/src/presentation/screens/switches/SwitchScreen.tsx
@@ -1,9 +1,38 @@
-import {View, Text} from 'react-native';
+import {useState} from 'react';
+import {View, Text, Switch} from 'react-native';
+import {CustomView} from '../../components/ui/CustomView';
+import {Card} from '../../components/ui/Card';
+import {CustomSwitch} from '../../components/ui/CustomSwitch';
 
 export const SwitchScreen = () => {
+  //     const [isEnabled, setIsEnabled] = useState(false);
+  //   const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+  const [state, setState] = useState({
+    isActive: true,
+    isHungry: false,
+    isHappy: true,
+  });
+
   return (
-    <View>
-      <Text>SwitchScreen</Text>
-    </View>
+    <CustomView style={{marginTop: 100, paddingHorizontal: 10}}>
+      <Card>
+        <CustomSwitch
+          isOn={state.isActive}
+          onChange={value => setState({...state, isActive: value})}
+          text="Activo"
+        />
+        <CustomSwitch
+          isOn={state.isHungry}
+          onChange={value => setState({...state, isHungry: value})}
+          text="Tiene hambre"
+        />
+        <CustomSwitch
+          isOn={state.isHappy}
+          onChange={value => setState({...state, isHappy: value})}
+          text="Está feliz"
+        />
+      </Card>
+    </CustomView>
   );
 };
