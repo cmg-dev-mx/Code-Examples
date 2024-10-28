60045250a10782a927fb269f52edcab6c3c3d4a4
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-10-28 13:46:31 -0600

    Ejemplo de pull to request

--- React-Native/08-ComponentsApp/src/presentation/navigator/Navigator.tsx
+++ React-Native/08-ComponentsApp/src/presentation/navigator/Navigator.tsx
@@ -5,6 +5,7 @@ import {Animation102Screen} from '../screens/animations/Animation102Screen';
 import {SwitchScreen} from '../screens/switches/SwitchScreen';
 import {AlertScreen} from '../screens/alerts/AlertScreen';
 import {TextInputScreen} from '../screens/inputs/TextInputScreen';
+import {PullToRefreshScreen} from '../screens/ui/PullToRefreshScreen';
 
 const Stack = createStackNavigator();
 
@@ -17,9 +18,15 @@ export const Navigator = () => {
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Animation101Screen" component={Animation101Screen} />
       <Stack.Screen name="Animation102Screen" component={Animation102Screen} />
+
       <Stack.Screen name="SwitchScreen" component={SwitchScreen} />
       <Stack.Screen name="AlertScreen" component={AlertScreen} />
       <Stack.Screen name="TextInputScreen" component={TextInputScreen} />
+
+      <Stack.Screen
+        name="PullToRefreshScreen"
+        component={PullToRefreshScreen}
+      />
     </Stack.Navigator>
   );
 };
