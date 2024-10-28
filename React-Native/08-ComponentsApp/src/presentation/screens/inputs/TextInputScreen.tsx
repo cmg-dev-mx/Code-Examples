c35d42f1017dba476f07a9179716a54aa850c70f
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-10-28 13:16:11 -0600

    Ejemplo de entradas de texto (text inputs)

---
+++ React-Native/08-ComponentsApp/src/presentation/screens/inputs/TextInputScreen.tsx
@@ -0,0 +1,52 @@
+import {Text, TextInput, View} from 'react-native';
+import {CustomView} from '../../components/ui/CustomView';
+import {Title} from '../../components/ui/Title';
+import {globalStyles} from '../../../config/theme/theme';
+import {Card} from '../../components/ui/Card';
+import {useState} from 'react';
+
+export const TextInputScreen = () => {
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    phone: '',
+  });
+
+  return (
+    <CustomView margin>
+      <Title text="TextInput" safe />
+
+      <Card>
+        <TextInput
+          style={globalStyles.input}
+          placeholder="Nombre completo"
+          autoCapitalize={'words'}
+          autoCorrect={false}
+          onChangeText={value => setForm({...form, name: value})}
+        />
+        <TextInput
+          style={globalStyles.input}
+          placeholder="Correo electrónico"
+          autoCapitalize={'none'}
+          autoCorrect={false}
+          keyboardType="email-address"
+          onChangeText={value => setForm({...form, email: value})}
+        />
+        <TextInput
+          style={globalStyles.input}
+          placeholder="Teléfono"
+          autoCapitalize={'none'}
+          autoCorrect={false}
+          keyboardType="phone-pad"
+          onChangeText={value => setForm({...form, phone: value})}
+        />
+      </Card>
+
+      <View style={{height: 10}} />
+
+      <Card>
+        <Text>{JSON.stringify(form, null, 2)}</Text>
+      </Card>
+    </CustomView>
+  );
+};
