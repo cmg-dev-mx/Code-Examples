027942e1780c684964b1e7902d9269842add73dc
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-11-04 18:51:33 -0700

    Refactor de clase

--- Dart/Ejercicios/05_clase/01_clase.dart
+++ Dart/Ejercicios/05_clase/01_clase.dart
@@ -1,23 +1,10 @@
+import 'clases/Persona.dart';
+
 main() {
-  Persona persona = Persona();
-  persona.nombre = 'Cesar';
-  persona.edad = 37;
-  persona.bio = 'Nació en la ciudad de México';
+  Persona persona = Persona()
+    ..nombre = 'Cesar'
+    ..edad = 37
+    ..bio = 'Nació en la ciudad de México';
 
   print(persona);
-}
-
-class Persona {
-  // Campos o propiedades
-  String? nombre;
-  int? edad;
-  String? bio;
-
-  // Get y Set
-
-  // Constructores
-
-  // Métodos
-  @override
-  String toString() => '$nombre tiene $edad años y su biografía es: $bio';
 }
---
+++ Dart/Ejercicios/05_clase/clases/Persona.dart
@@ -0,0 +1,14 @@
+class Persona {
+  // Campos o propiedades
+  String? nombre;
+  int? edad;
+  String? bio;
+
+  // Get y Set
+
+  // Constructores
+
+  // Métodos
+  @override
+  String toString() => '$nombre tiene $edad años y su biografía es: $bio';
+}
