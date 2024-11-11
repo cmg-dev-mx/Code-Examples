081ed9b8f64759a4c1be06537eade599980fc2b2
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-11-11 14:18:31 -0700

    Implementación de pantalla Búsqueda

--- React-Native/09-pokedexApp/src/presentation/screens/search/SearchScreen.tsx
+++ React-Native/09-pokedexApp/src/presentation/screens/search/SearchScreen.tsx
@@ -1,9 +1,40 @@
-import {Text, View} from 'react-native';
+import {FlatList, View} from 'react-native';
+import {globalTheme} from '../../../config/theme/global-theme';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {ActivityIndicator, FAB, Text, TextInput} from 'react-native-paper';
+import {PokemonCard} from '../../components/pokemons/PokemonCard';
+import {Pokemon} from '../../../domain/entities/pokemon';
 
 export const SearchScreen = () => {
+  const {top} = useSafeAreaInsets();
   return (
-    <View>
-      <Text>SearchScreen</Text>
+    <View
+      style={[
+        globalTheme.globalMargin,
+        {
+          paddingTop: top + 20,
+        },
+      ]}>
+      <TextInput
+        placeholder="Buscar Pokemon"
+        mode="flat"
+        autoFocus
+        autoCorrect={false}
+        onChangeText={value => console.log(value)}
+        value={''}
+      />
+
+      <ActivityIndicator style={{paddingTop: 20}} />
+
+      <FlatList
+        data={[] as Pokemon[]}
+        keyExtractor={(pokemon, index) => `${pokemon.id}-${index}`}
+        numColumns={2}
+        style={{paddingTop: top + 20}}
+        renderItem={({item}) => <PokemonCard pokemon={item} />}
+        onEndReachedThreshold={0.7}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 };
