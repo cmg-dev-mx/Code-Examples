192a7f004eee692159168c8c761cf39bf9c0a08e
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-11-10 19:26:59 -0700

    Adición de animación para mostrar imagen

--- React-Native/09-pokedexApp/src/presentation/components/pokemons/PokemonCard.tsx
+++ React-Native/09-pokedexApp/src/presentation/components/pokemons/PokemonCard.tsx
@@ -1,6 +1,7 @@
 import {Image, StyleSheet, View} from 'react-native';
 import {Pokemon} from '../../../domain/entities/pokemon';
 import {Card, Text} from 'react-native-paper';
+import {FadeInImage} from '../ui/FadeInImage';
 
 interface Props {
   pokemon: Pokemon;
@@ -21,11 +22,7 @@ export const PokemonCard = ({pokemon}: Props) => {
         />
       </View>
 
-      <Image
-        source={{uri: pokemon.avatar}}
-        style={styles.pokemonImage}
-        resizeMode="contain"
-      />
+      <FadeInImage uri={pokemon.avatar} style={styles.pokemonImage} />
 
       <Text style={[styles.name, {marginTop: 35}]}>{pokemon.types[0]}</Text>
     </Card>
