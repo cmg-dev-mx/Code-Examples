c88492c1805f1a37c9f88523c51b96502477c422
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-12-05 11:14:10 -0700

    Extracción de info en el body de la respuesta

--- Dart/paquetes/bin/paquetes.dart
+++ Dart/paquetes/bin/paquetes.dart
@@ -4,6 +4,8 @@
  * Tercero las importaciones de paquetes propios
  */
 
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 import 'package:paquetes/paquetes.dart' as paquetes;
@@ -11,6 +13,10 @@ import 'package:paquetes/paquetes.dart' as paquetes;
 void main(List<String> arguments) {
   final uri = Uri.parse('https://reqres.in/api/users?page=2');
   http.get(uri).then((res) {
-    print(res);
+    final body = jsonDecode(res.body);
+    print(body);
+    print('page: ${body['page']}');
+    print('per_page: ${body['per_page']}');
+    print('id del 3er elemento: ${body['data'][2]['id']}');
   });
 }
