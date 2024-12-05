4467ccf1073ddfeb6f16cb2a6e8aa3e3b5131ddf
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-12-05 11:06:47 -0700

    Adición de librería HTTP y llamada de ejemplo

---
+++ Dart/README.md
@@ -0,0 +1,13 @@
+# Instalación de paquetes en Dart
+
+Acceder a la página de gestión de paquetes oficial de Dart: [pub.dev](https://pub.dev/)
+
+## Paquetes instalados
+
+- [http](https://pub.dev/packages/http): Librería para realizar peticiones HTTP.
+
+1. Correr el siguiente comando en la terminal para instalar el paquete http:
+
+```bash
+dart pub add http
+```
--- Dart/paquetes/bin/paquetes.dart
+++ Dart/paquetes/bin/paquetes.dart
@@ -1,5 +1,16 @@
+/**
+ * Primero van las importaciones de paquetes de google
+ * Segundo las importaciones de paquetes de terceros
+ * Tercero las importaciones de paquetes propios
+ */
+
+import 'package:http/http.dart' as http;
+
 import 'package:paquetes/paquetes.dart' as paquetes;
 
 void main(List<String> arguments) {
-  print('Hello world: ${paquetes.calculate()}!');
+  final uri = Uri.parse('https://reqres.in/api/users?page=2');
+  http.get(uri).then((res) {
+    print(res);
+  });
 }
