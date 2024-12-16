3f0b4094abec3e290f894381e8f875743f698858
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-12-16 15:25:42 -0700

    Implementación para mover la cámara a la última ubicación

--- React-Native/10-MapsApp/src/presentation/components/maps/Map.tsx
+++ React-Native/10-MapsApp/src/presentation/components/maps/Map.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {Platform} from 'react-native';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import {Location} from '../../../infrastructure/interfaces/location';
 import {FAB} from '../ui/FAB';
+import {useLocationStore} from '../../store/location/useLocationStore';
 
 interface Props {
   showUserLocation?: boolean;
@@ -10,15 +11,41 @@ interface Props {
 }
 
 export const Map = ({showUserLocation = true, initialLocation}: Props) => {
+  const mapRef = useRef<MapView>();
+  const cameraLocation = useRef<Location>(initialLocation);
+  const {getLocation, lastKnownLocation} = useLocationStore();
+
+  const moveCameraToLocation = (location: Location) => {
+    if (!mapRef.current) {
+      return;
+    }
+
+    mapRef.current.animateCamera({
+      center: location,
+    });
+  };
+
+  const moveToCurrentLocation = async () => {
+    if (!lastKnownLocation) {
+      moveCameraToLocation(initialLocation);
+    }
+    const location = await getLocation();
+    if (!location) {
+      return;
+    }
+    moveCameraToLocation(location);
+  };
+
   return (
     <>
       <MapView
+        ref={map => (mapRef.current = map!)}
         showsUserLocation={showUserLocation}
         provider={Platform.OS === 'ios' ? undefined : PROVIDER_GOOGLE}
         style={{flex: 1}}
         region={{
-          latitude: initialLocation.latitude,
-          longitude: initialLocation.longitude,
+          latitude: cameraLocation.current.latitude,
+          longitude: cameraLocation.current.longitude,
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
         }}>
@@ -35,7 +62,7 @@ export const Map = ({showUserLocation = true, initialLocation}: Props) => {
 
       <FAB
         iconName="compass-outline"
-        onPress={() => console.log('FAB pressed')}
+        onPress={moveToCurrentLocation}
         style={{
           bottom: 20,
           right: 20,
