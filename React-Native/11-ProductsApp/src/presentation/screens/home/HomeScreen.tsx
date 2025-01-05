bb91075991344a5f24ebcc69f25e3339a699f9a3
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2025-01-04 19:34:57 -0700

    Implementación de botón flotante para nuevo producto

--- React-Native/11-ProductsApp/src/presentation/screens/home/HomeScreen.tsx
+++ React-Native/11-ProductsApp/src/presentation/screens/home/HomeScreen.tsx
@@ -1,13 +1,17 @@
-import {Layout, Text} from '@ui-kitten/components';
+import React from 'react';
 import {useAuthStore} from '../../store/auth/useAuthStore';
 import {getProductsByPage} from '../../../actions/products/get-products-by-page';
 import {useInfiniteQuery, useQuery} from '@tanstack/react-query';
 import {MainLayout} from '../../layouts/MainLayout';
 import {FullScreenLoader} from '../../components/ui/FullScreenLoader';
 import {ProductList} from '../../components/products/ProductList';
+import {FAB} from '../../components/ui/FAB';
+import {NavigationProp, useNavigation} from '@react-navigation/native';
+import {RootStackParams} from '../../navigation/StackNavigator';
 
 export const HomeScreen = () => {
   const {logout} = useAuthStore();
+  const navigation = useNavigation<NavigationProp<RootStackParams>>();
 
   const {isLoading, data, fetchNextPage} = useInfiniteQuery({
     queryKey: ['products', 'infinite'],
@@ -18,17 +22,30 @@ export const HomeScreen = () => {
   });
 
   return (
-    <MainLayout
-      title="TesloShop - Productos"
-      subTitle="Aplicación administrativa">
-      {isLoading ? (
-        <FullScreenLoader />
-      ) : (
-        <ProductList
-          products={data?.pages.flat() ?? []}
-          fetchNextPage={fetchNextPage}
-        />
-      )}
-    </MainLayout>
+    <>
+      <MainLayout
+        title="TesloShop - Productos"
+        subTitle="Aplicación administrativa">
+        {isLoading ? (
+          <FullScreenLoader />
+        ) : (
+          <ProductList
+            products={data?.pages.flat() ?? []}
+            fetchNextPage={fetchNextPage}
+          />
+        )}
+      </MainLayout>
+      <FAB
+        iconName="plus-outline"
+        style={{
+          position: 'absolute',
+          bottom: 32,
+          right: 32,
+        }}
+        onPress={() => {
+          navigation.navigate('ProductScreen', {productId: 'new'});
+        }}
+      />
+    </>
   );
 };
