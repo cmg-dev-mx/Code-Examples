920e9667bf6162dd608ca2f2708a879727e02062
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2025-01-04 19:19:21 -0700

    Implementación de invalidación de caché al crear o actualizar

--- React-Native/11-ProductsApp/src/presentation/screens/product/ProductScreen.tsx
+++ React-Native/11-ProductsApp/src/presentation/screens/product/ProductScreen.tsx
@@ -7,7 +7,7 @@ import {
   useTheme,
 } from '@ui-kitten/components';
 import {MainLayout} from '../../layouts/MainLayout';
-import {useMutation, useQuery} from '@tanstack/react-query';
+import {useMutation, useQuery, useQueryClient} from '@tanstack/react-query';
 import {StackScreenProps} from '@react-navigation/stack';
 import {RootStackParams} from '../../navigation/StackNavigator';
 import {getProductById} from '../../../actions/products/get-product-by-id';
@@ -27,6 +27,7 @@ interface Props extends StackScreenProps<RootStackParams, 'ProductScreen'> {}
 export const ProductScreen = ({route}: Props) => {
   const productIdRef = useRef(route.params.productId);
   const theme = useTheme();
+  const queryClient = useQueryClient();
 
   const {data: product} = useQuery({
     queryKey: ['product', productIdRef.current],
@@ -37,7 +38,9 @@ export const ProductScreen = ({route}: Props) => {
     mutationFn: (data: Product) =>
       updateCreateProduct({...data, id: productIdRef.current}),
     onSuccess(data: Product) {
-      console.log('Success');
+      productIdRef.current = data.id;
+      queryClient.invalidateQueries({queryKey: ['products', 'infinite']});
+      queryClient.invalidateQueries({queryKey: ['product', data.id]});
     },
   });
 
