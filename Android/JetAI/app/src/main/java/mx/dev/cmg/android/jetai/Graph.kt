b5235753562bc882296fb528dabdd27efe5ca135
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2025-02-04 13:26:57 -0700

    Implementación de repositorio para chat

--- Android/JetAI/app/src/main/java/mx/dev/cmg/android/jetai/Graph.kt
+++ Android/JetAI/app/src/main/java/mx/dev/cmg/android/jetai/Graph.kt
@@ -1,6 +1,8 @@
 package mx.dev.cmg.android.jetai
 
 import android.content.Context
+import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.generationConfig
 import com.google.android.gms.auth.api.identity.Identity
 import mx.dev.cmg.android.jetai.data.repository.AuthRepository
 import mx.dev.cmg.android.jetai.data.repository.AuthRepositoryImpl
@@ -13,6 +15,16 @@ object Graph {
     }
 
     lateinit var googleAuthClient: GoogleAuthClient
+
+    private val config = generationConfig {
+        temperature = 0.7f
+    }
+
+    fun generativeModel(modelName: String) = GenerativeModel(
+        modelName = modelName,
+        apiKey = BuildConfig.GEMINI_API_KEY,
+        generationConfig = config
+    )
 
     fun provide(context: Context) {
         googleAuthClient = GoogleAuthClient(
