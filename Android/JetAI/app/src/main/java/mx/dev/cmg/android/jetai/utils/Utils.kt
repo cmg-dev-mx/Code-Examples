c79c0897664e9aa08117015e4ebd70eb6ce3fe5d
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2025-02-04 14:23:59 -0700

    Implementación de vista de cuarto de chats

--- Android/JetAI/app/src/main/java/mx/dev/cmg/android/jetai/utils/Utils.kt
+++ Android/JetAI/app/src/main/java/mx/dev/cmg/android/jetai/utils/Utils.kt
@@ -1,9 +1,18 @@
 package mx.dev.cmg.android.jetai.utils
 
 import android.util.Patterns
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 fun isValidEmail(email: String): Boolean {
     if (email.isEmpty()) return false
     val emailPattern = Patterns.EMAIL_ADDRESS
     return emailPattern.matcher(email).matches()
+}
+
+fun formatDate(date: Date): String {
+    val pattern = "yyyy-MM-dd HH:mm:ss"
+    val sdf = SimpleDateFormat(pattern, Locale.getDefault())
+    return sdf.format(date)
 }
