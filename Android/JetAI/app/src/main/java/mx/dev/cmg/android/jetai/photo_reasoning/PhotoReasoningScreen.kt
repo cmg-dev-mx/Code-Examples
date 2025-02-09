561e693e91905700961c1d778bce6fe5d2ec88f5
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2025-02-09 14:31:12 -0700

    Implementación de la pantalla de razonamiento fotográfico con carga y gestión de imágenes

---
+++ Android/JetAI/app/src/main/java/mx/dev/cmg/android/jetai/photo_reasoning/PhotoReasoningScreen.kt
@@ -0,0 +1,287 @@
+package mx.dev.cmg.android.jetai.photo_reasoning
+
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.launch
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.ImageLoader
+import coil.request.ImageRequest
+import coil3.compose.AsyncImage
+import mx.dev.cmg.android.jetai.R
+import mx.dev.cmg.android.jetai.utils.saveImageToFileAndGetUri
+
+@Composable
+fun PhotoReasoningScreen(
+    modifier: Modifier = Modifier,
+    viewModel: PhotoReasoningViewModel = viewModel()
+) {
+    val photoReasoningUiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val coroutineScope = rememberCoroutineScope()
+    val imageRequestBuilder = ImageRequest.Builder(LocalContext.current)
+    val imageLoader = ImageLoader.Builder(LocalContext.current).build()
+}
+
+@Composable
+private fun PhotoReasoningScreen(
+    modifier: Modifier = Modifier,
+    uiState: PhotoReasoningUiState,
+    onReasonClicked: (String, List<Uri>) -> Unit = { _, _ -> }
+) {
+    var userQuestion: TextFieldValue by rememberSaveable(
+        stateSaver = TextFieldValue.Saver
+    ) { mutableStateOf(TextFieldValue()) }
+    val imageUris = rememberSaveable(saver = UriSaver()) { mutableStateListOf() }
+    val context = LocalContext.current
+
+    val takePhotoLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.TakePicturePreview()
+    ) { bitmap ->
+        bitmap?.let {
+            val imageUri = saveImageToFileAndGetUri(bitmap, context)
+            imageUri?.let { imageUris.add(it) }
+        }
+    }
+
+    val pickMedia = rememberLauncherForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { imageUri ->
+        imageUri?.let { imageUris.add(imageUri) }
+    }
+
+    Column(
+        modifier = modifier.padding(16.dp)
+    ) {
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.surface
+            ),
+            elevation = CardDefaults.cardElevation(3.dp)
+        ) {
+            Row(
+                modifier = Modifier.padding(top = 16.dp)
+            ) {
+                UserInputText(
+                    textFieldValue = userQuestion,
+                    onTextChanged = { userQuestion = it },
+                    focusState = false,
+                    onTextFieldFocused = {},
+                    modifier = Modifier.fillMaxWidth(.8f)
+                )
+
+                IconButton(
+                    onClick = {
+                        if (userQuestion.text.isNotBlank()) {
+                            onReasonClicked(userQuestion.text, imageUris.toList())
+                        }
+                    },
+                    modifier = Modifier
+                        .padding(start = 4.dp)
+                        .align(Alignment.CenterVertically)
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_send),
+                        contentDescription = null
+                    )
+                }
+            }
+
+            Row(
+                modifier = Modifier.padding(top = 16.dp)
+            ) {
+                IconButton(
+                    onClick = {
+                        pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+                    },
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .align(Alignment.CenterVertically)
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_photo),
+                        contentDescription = null
+                    )
+                }
+
+                IconButton(
+                    onClick = {
+                        takePhotoLauncher.launch()
+                    },
+                    modifier = Modifier
+                        .padding(start = 4.dp)
+                        .align(Alignment.CenterVertically)
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_image),
+                        contentDescription = null
+                    )
+                }
+            }
+
+            LazyRow(
+                modifier = Modifier.padding(8.dp)
+            ) {
+                items(imageUris) { imageUri ->
+                    Column {
+                        Surface(
+                            shape = CircleShape,
+                            color = MaterialTheme.colorScheme.surfaceVariant.copy(
+                                alpha = .5f
+                            ),
+                            onClick = {
+                                imageUris.remove(imageUri)
+                            },
+                            modifier = Modifier.align(Alignment.End)
+                        ) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_remove),
+                                contentDescription = null
+                            )
+                        }
+                        AsyncImage(
+                            model = imageUri,
+                            contentDescription = null,
+                            modifier = Modifier
+                                .padding(
+                                    top = 0.dp,
+                                    bottom = 4.dp,
+                                    start = 4.dp,
+                                    end = 4.dp
+                                )
+                                .requiredSize(102.dp)
+                                .clip(RoundedCornerShape(20.dp))
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun UserInputText(
+    modifier: Modifier = Modifier,
+    textFieldValue: TextFieldValue,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    onTextChanged: (TextFieldValue) -> Unit,
+    onTextFieldFocused: (Boolean) -> Unit,
+    focusState: Boolean
+) {
+    val labelText = "Text here"
+    Row(
+        modifier = modifier.height(64.dp),
+        horizontalArrangement = Arrangement.End
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            UserInputTextField(
+                textFieldValue = textFieldValue,
+                onTextChanged = onTextChanged,
+                onTextFieldFocused = onTextFieldFocused,
+                keyboardType = keyboardType,
+                focusState = focusState,
+                modifier = Modifier.semantics {
+                    contentDescription = labelText
+                }
+            )
+        }
+    }
+}
+
+@Composable
+private fun BoxScope.UserInputTextField(
+    textFieldValue: TextFieldValue,
+    onTextChanged: (TextFieldValue) -> Unit,
+    onTextFieldFocused: (Boolean) -> Unit,
+    keyboardType: KeyboardType,
+    focusState: Boolean,
+    modifier: Modifier = Modifier
+) {
+    var lastFocusState by remember { mutableStateOf(false) }
+    BasicTextField(
+        value = textFieldValue,
+        onValueChange = { onTextChanged(it) },
+        modifier = modifier
+            .padding(start = 32.dp)
+            .align(Alignment.CenterStart)
+            .onFocusChanged { state ->
+                if (lastFocusState != state.isFocused) {
+                    onTextFieldFocused(state.isFocused)
+                }
+                lastFocusState = state.isFocused
+            },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = keyboardType,
+            imeAction = ImeAction.Send
+        ),
+        maxLines = 1,
+        cursorBrush = SolidColor(LocalContentColor.current),
+        textStyle = LocalTextStyle.current.copy(
+            color = LocalContentColor.current
+        )
+    )
+
+    val disabledColor = MaterialTheme.colorScheme.onSurfaceVariant
+
+    if (textFieldValue.text.isEmpty() && !focusState) {
+        Text(
+            modifier = Modifier
+                .align(Alignment.CenterStart)
+                .padding(start = 32.dp),
+            text = "Write a prompt",
+            style = MaterialTheme.typography.bodyLarge.copy(
+                color = disabledColor
+            )
+        )
+    }
+}
--- Android/JetAI/app/src/main/java/mx/dev/cmg/android/jetai/utils/Utils.kt
+++ Android/JetAI/app/src/main/java/mx/dev/cmg/android/jetai/utils/Utils.kt
@@ -1,6 +1,13 @@
 package mx.dev.cmg.android.jetai.utils
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Environment
 import android.util.Patterns
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -15,4 +22,23 @@ fun formatDate(date: Date): String {
     val pattern = "yyyy-MM-dd HH:mm:ss"
     val sdf = SimpleDateFormat(pattern, Locale.getDefault())
     return sdf.format(date)
+}
+
+fun saveImageToFileAndGetUri(bitmap: Bitmap, context: Context): Uri? {
+    val fileName = "${System.currentTimeMillis()}.jpg"
+    val imagesDirectory = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+    val imageFile = File(imagesDirectory, fileName)
+    var fos: OutputStream? = null
+
+    try {
+        fos = FileOutputStream(imageFile)
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
+        fos.flush()
+        return Uri.fromFile(imageFile)
+    } catch (e: Exception) {
+        e.printStackTrace()
+    } finally {
+        fos?.close()
+    }
+    return null
 }
